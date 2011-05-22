ae6e556f2bb98699f0652d8c3e70dabb76e5da75
Mark Heath <mark.heath@gmail.com>
2011-05-22 22:40:44 +0100

    Added unit test assembly and first unit test

---
+++ ModularWPFUnitTests/MainWindowViewModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModularWPFTest;
+
+namespace ModularWPFUnitTests
+{
+    [TestClass]
+    public class MainWindowViewModelTests
+    {
+        class FakeModule : IModule
+        {
+
+            public string Name
+            {
+                get { return "Fake Module"; }
+            }
+
+            public System.Windows.Controls.UserControl UserInterface
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+        [TestMethod]
+        public void FirstModuleShouldBeSelectedByDefault()
+        {
+            FakeModule module = new FakeModule();
+            MainWindowViewModel vm = new MainWindowViewModel(new FakeModule[] { module });
+
+        }
+    }
+}
