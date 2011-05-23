2439ead0b630914adef62bd47862d2d63b2787c0
Mark Heath <mark.heath@gmail.com>
2011-05-23 23:50:00 +0100

    Moved module1 stuff into its own folder

---
+++ ModularWPFTest/Module1/Module1ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ModularWPFTest
+{
+    class Module1ViewModel : INotifyPropertyChanged
+    {
+        private string userName;
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                if (userName != value)
+                {
+                    userName = value;
+                    RaisePropertyChanged("UserName");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
