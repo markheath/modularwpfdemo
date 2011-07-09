1cb3b52842fee4bfb3f4afd2a47ebc865615f142
Mark Heath <mark.heath@gmail.com>
2011-07-09 22:34:38 +0100

    Implemented CanExit for Module1

--- ModularWPFTest/Module1/Module1.cs
+++ ModularWPFTest/Module1/Module1.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Windows.Controls;
 using System.ComponentModel.Composition;
+using System.Windows;
 
 namespace ModularWPFTest
 {
     [Export(typeof(IModule))]
     class Module1 : IModule
     {
+        private readonly Module1ViewModel viewModel = new Module1ViewModel();
+
         public string Name
         {
             get { return "Module 1"; }
@@ -20,14 +23,28 @@ namespace ModularWPFTest
             get 
             { 
                 var view = new Module1View();
-                view.DataContext = new Module1ViewModel();
+                view.DataContext = viewModel;
                 return view;
             }
         }
 
         public bool CanExit
         {
-            get { throw new NotImplementedException(); }
+            get 
+            {
+                bool canExit = true;
+                if (!String.IsNullOrEmpty(viewModel.UserName))
+                {
+                    var dialogResult = MessageBox.Show("You have not saved your changes, are you sure you want to navigate away?",
+                        "Exiting Module 1",
+                        MessageBoxButton.YesNoCancel);
+                    if (dialogResult != MessageBoxResult.Yes)
+                    {
+                        canExit = false;
+                    }
+                }
+                return canExit;
+            }
         }
     }
 }
